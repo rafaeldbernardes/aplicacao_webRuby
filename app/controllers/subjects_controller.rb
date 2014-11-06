e087cef97eaf3824a12c1016beaf6b3e15c2d9d8
rafaeldbernardes <rafaeldbernardes@gmail.com>
2014-11-06 19:46:41 -0200

    Commit Projeto Inicial

---
+++ app/controllers/subjects_controller.rb
@@ -0,0 +1,66 @@
+class SubjectsController < ApplicationController
+
+  layout "admin"
+
+  def index
+    @subjects = Subject.sorted
+  end
+
+  def show
+    @subject = Subject.find(params[:id])
+  end
+
+  def new
+    @subject = Subject.new({:name => "Rafael"})
+  end
+
+  def create
+    # Instantiate a new object using form parameters
+    @subject = Subject.new(subject_params)
+    @subject.save
+    # Save the object
+    if @subject.save
+    # If save succeeds, redirect to the index action
+    flash[:notice] = "Subject Created successfully"
+      redirect_to(:action => 'index')
+    else
+    # If save fails, redisplay the form so user can fix problems      
+      render('new')
+    end 
+  end
+
+  def edit
+    @subject = Subject.find(params[:id])
+  end
+
+  def update
+    # Fiding an existing object using form parameters
+    @subject = Subject.find(params[:id])
+     # Update the object
+    if @subject.update_attributes(subject_params)
+    # If update succeeds, redirect to the index action
+    flash[:notice] = "Subject Updated successfully"
+      redirect_to(:action => 'show', :id => @subject.id)
+    else
+    # If update fails, redisplay the form so user can fix problems      
+      render('edit')
+    end 
+  end
+
+  def delete
+    @subject = Subject.find(params[:id])
+  end
+
+  def destroy
+    subject = Subject.find(params[:id]).destroy
+    flash[:notice] = "Subject destroyed successfully"
+    redirect_to(:action => 'index')
+  end
+
+  private 
+
+    def subject_params
+      params.require(:subject).permit(:name, :position, :visible)
+    end
+
+end
